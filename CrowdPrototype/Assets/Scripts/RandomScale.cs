79bd936dcbc478984f38a3371ad50fcffffd085f
Maxime Houis <maxime.houis@epitech.eu>
2021-01-28 01:34:19 +0100

    feat: ants and random spawner

---
+++ CrowdPrototype/Assets/Scripts/RandomScale.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomScale : MonoBehaviour
+{
+    [Header("Scale")]
+    [Min(0)] public float m_Minimum = 0;
+
+    [Min(0)] public float m_Maximum = 1;
+
+    void Start()
+    {
+        transform.localScale = Random.Range(m_Minimum, m_Maximum) * Vector3.one;
+    }
+}
---
+++ CrowdPrototype/Assets/Scripts/Spawner.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+[RequireComponent(typeof(BoxCollider))]
+public class AntSpawner : MonoBehaviour
+{
+    public GameObject m_entity;
+    [Min(0)] public int m_count = 25;
+    [Min(0)] public float m_range = 2.5f;
+
+    private GameObject m_group;
+    private BoxCollider m_spawnBox;
+
+    void Start()
+    {
+        m_group = Instantiate(new GameObject("Ant Group"));
+        m_spawnBox = GetComponent<BoxCollider>();
+
+        StartCoroutine(DoSpawn());
+    }
+
+    private IEnumerator DoSpawn()
+    {
+        for (var i = 0; i < m_count; i++)
+        {
+            var pos = transform.position;
+            pos.x += Random.Range(-m_range, m_range);
+            pos.z += Random.Range(-m_range, m_range);
+
+            var entity = Instantiate(m_entity, pos, Quaternion.identity, m_group.transform);
+            entity.name = "Ant #" + (i + 1);
+
+            yield return new WaitForSeconds(0.025f);
+        }
+    }
+}
