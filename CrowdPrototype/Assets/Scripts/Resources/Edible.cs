5126f4de5ab345102da3802aebf40d9fe9d83dae
Maxime Houis <maxime.houis@epitech.eu>
2021-02-11 23:20:50 +0100

    feat: add draft for edibles and materials

---
+++ CrowdPrototype/Assets/Scripts/Resources/Edible.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class Edible : MonoBehaviour
+{
+    [Tooltip("Can be negative")]
+    public int m_value;
+
+    [Min(0)] [Tooltip("In seconds")]
+    public float m_eatingTime = 1f;
+
+    private bool m_free = true;
+
+    public bool Eat(AntController antController)
+    {
+        if (!m_free)
+            return false;
+
+        m_free = false;
+        // TODO: eating stuff
+
+        return true;
+    }
+}
---
+++ CrowdPrototype/Assets/Scripts/Resources/Material.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+public abstract class Material : MonoBehaviour
+{
+    [Min(0)] [Tooltip("In seconds")]
+    public float m_collectTime = 1f;
+}
