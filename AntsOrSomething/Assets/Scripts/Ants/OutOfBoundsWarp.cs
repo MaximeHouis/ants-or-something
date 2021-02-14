95a18b691e6887136a19e5a79c49aeccc6be89dc
Maxime Houis <maxime.houis@epitech.eu>
2021-02-14 15:09:45 +0100

    feat: add out of bounds warp script

---
+++ AntsOrSomething/Assets/Scripts/Ants/OutOfBoundsWarp.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class OutOfBoundsWarp : MonoBehaviour
+{
+    public Transform m_respawnPoint;
+    public float m_yWarpLimit = -42f;
+
+    private void FixedUpdate()
+    {
+        if (transform.position.y <= m_yWarpLimit)
+            transform.position = m_respawnPoint.position;
+    }
+}
