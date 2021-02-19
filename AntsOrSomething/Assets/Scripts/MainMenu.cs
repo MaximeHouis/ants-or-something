85782fea583213afd2065d694e52ebf9886d72ca
Maxime Houis <maxime.houis@epitech.eu>
2021-02-19 21:25:23 +0100

    feat: main menu

---
+++ AntsOrSomething/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+
+    public Text m_versionText;
+
+    private void Awake()
+    {
+        m_versionText.text = Application.version;
+    }
+
+    public void Map1()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Map2()
+    {
+        SceneManager.LoadScene(2);
+    }
+    
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
