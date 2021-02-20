793486feada7b8aae38433892caf12a7388dcaf9
Maxime Houis <maxime.houis@epitech.eu>
2021-02-20 18:58:00 +0100

    feat: pause menu

--- AntsOrSomething/Assets/Scripts/GUI/GameUI.cs
+++ AntsOrSomething/Assets/Scripts/GUI/GameUI.cs
@@ -7,9 +7,13 @@ public class GameUI : MonoBehaviour
 
     public Text TimerText;
     public Text CountdownText;
+    public GameObject m_pauseMenuPrefab;
 
     private AntPlayer m_antPlayer;
     private CheckpointTracker m_playerTracker;
+
+    private bool m_paused;
+    private GameObject m_pauseMenu;
 
     private void Start()
     {
@@ -21,10 +25,28 @@ public class GameUI : MonoBehaviour
 
     public void Update()
     {
+        if (Input.GetButtonDown("Menu"))
+            TogglePause();
+
         var reference = !m_playerTracker.Finished
             ? CheckpointSystem.Instance.Elapsed
             : m_playerTracker.CurrentTime;
 
         TimerText.text = "Time: " + reference.ToString("c");
+    }
+
+    public void TogglePause()
+    {
+        if (m_paused)
+        {
+            Destroy(m_pauseMenu);
+        }
+        else
+        {
+            m_pauseMenu = Instantiate(m_pauseMenuPrefab);
+        }
+
+        m_paused = !m_paused;
+        Time.timeScale = m_paused ? 0f : 1f;
     }
 }
---
+++ AntsOrSomething/Assets/Scripts/GUI/PauseMenu.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public void Resume()
+    {
+        GameUI.Instance.TogglePause();
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
